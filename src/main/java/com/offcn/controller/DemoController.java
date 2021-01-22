d41685719f400f51e70d6b55af4b2dd6d19ffb9f
dx <3027896944@qq.com>
2021-01-22 15:07:44 +0800

    master

---
+++ src/main/java/com/offcn/controller/DemoController.java
@@ -0,0 +1,10 @@
+package com.offcn.controller;
+
+public class DemoController {
+    public void demo(){
+        System.out.println("master");
+    }
+    public void test(){
+        System.out.println("master2");
+    }
+}
